c07a4a6a42d01705f6d5e2c646523c48b6bfc133
Joonas Hamunen <joonas.hamunen@feedbackly.com>
2021-09-17 13:12:14 +0200

    +

--- .github/actions/js/index.js
+++ .github/actions/js/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 console.log(process.cwd())
 
-const file = fs.readFileSync("../../../kustomize/build1/deployment.yml").toString();
+const file = fs.readFileSync("./kustomize/build1/deployment.yml").toString();
 
 console.log(file)
